3d6a3e9dfb26f5e58fce39220f43ecc188191219
Fagbayibo <fagbayibooluwasegun@gmail.com>
2025-09-26 16:32:07 +0100

    Updated Verfication ROute

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,26 +6,24 @@ import Login from "../auth/Login";
 import Home from "../pages/public/Home";
 import SignUp from "../auth/SignUp";
 import EmailOtpPage from "../auth/EmailOtpPage";
-import SmsOtpPage from '../auth/SmsOtpPage';
-import VerificationPage from "../auth/verification";
+import SmsOtpPage from "../auth/SmsOtpPage";
+import VerificationPage from "../auth/Verification";
 import ResetPasswordPage from "../auth/ResetPassword";
 import ForgotPasswordPage from "../auth/ForgotPassword";
-
 
 const AppRoutes = () => {
   return (
     <Routes>
       {/* Public Routes */}
-      <Route path="/" element={<Home/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/signup" element={<SignUp/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/email-otp" element={<EmailOtpPage />} />
       <Route path="/sms-otp" element={<SmsOtpPage />} />
       <Route path="/verify" element={<VerificationPage />} />
       <Route path="/reset-password" element={<ResetPasswordPage />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
       <Route path="/dashboard-home" element={<DashboardHome />} />
-
 
       {/* Protected Routes */}
       <Route
