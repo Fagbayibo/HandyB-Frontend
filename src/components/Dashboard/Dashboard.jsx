75c5fe584cc1c3e74d92970bdd30eb618f466130
Fagbayibo <fagbayibooluwasegun@gmail.com>
2025-09-26 16:00:50 +0100

    Removed render.yaml

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
-import react from "react";
+
 
 export const Dashboard = () => {
-    return
-      <div className="bg-wh">Dashboard</div>;
+    return (
+      <div className="bg-wh">Dashboard</div>
+    )
 }
